da44c01cfa8c992693bf02dbd5f3e9d6edcaf2ef
Song Eun Seok <eunsuk93@naver.com>
2021-05-19 17:54:57 +0900

    refactor : ,,,문제 해결

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,6 +10,9 @@ public class InputView {
     public List<Car> getCarNames(Scanner scanner) {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] cars = scanner.nextLine().split(",");
+        if (cars.length == 0) {
+            throw new IllegalArgumentException("자동차를 제대로 입력해주세요!");
+        }
         return GameSystem.makeCarList(cars);
     }
 
